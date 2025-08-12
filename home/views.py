e99c388010103b497aa2a03a4132398597b374c6
umesh jadhav <115002233+rjumesh@users.noreply.github.com>
2025-08-12 21:56:50 +0530

    29175

--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from .models import Restaurant
 class homepage(request):
     restaurant = Restaurant.objects.first()
     return render(request, 'homepage.html', {'restaurant_name' : restaurant.name if restaurant else 'my Restaurant'})
+
+def about_page(request):
+    return render(request, 'about.html')
+    
