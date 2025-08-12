0558320b2f2e5daf0f73c58cb4e810ddbf8b1f50
umesh jadhav <115002233+rjumesh@users.noreply.github.com>
2025-08-12 18:58:46 +0530

    29174

--- home/views.py
+++ home/views.py
@@ -1,6 +1,1 @@
 from django.shortcuts import render
-from .models import Restaurant
-
-def homepage(request):
-    restaurant = Restaurant.objrcts.first()
-    return render(request, 'homepage.html', {'restaurant_name': restaurant.name if restaurant else 'My Restaurant'})
