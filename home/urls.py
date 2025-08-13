a44b573a064441bd24cd690740427822a893d9c2
umesh jadhav <115002233+rjumesh@users.noreply.github.com>
2025-08-13 21:42:19 +0530

    31431

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('about/'',views.about_page, name='about'),    
     path('menu/'',views.menu_list, name='menu'),    
+    path('contact/'',views.contact, name='contact'),
 ]
