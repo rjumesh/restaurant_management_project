f01eb527df67e2af150a0945a43404afd79cff58
umesh jadhav <115002233+rjumesh@users.noreply.github.com>
2025-08-13 15:48:08 +0530

    31429

--- home/views.py
+++ home/views.py
@@ -8,3 +8,11 @@ class homepage(request):
 def about_page(request):
     return render(request, 'about.html')
     
+def menu_list(request):
+    menu_lists = [
+        {"name": "margherita pizza", "price":250, "description": "classic pizza with tomato and mozzarella"}
+        {"name": "veg burger", "price":150, "description": "classic burger with tomato and mozzarella"}
+        {"name": "pasta", "price":300, "description": "classic pasta"}
+        {"name": "Paneer tikka", "price":250, "description": "classic paneer tikka with spices"}
+    ] 
+    return render(request, 'menuview.html', {"menu_items": menu_items})
