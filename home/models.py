6bea3274a5ee3919e1b92fe39d7043f464386cf1
umesh jadhav <115002233+rjumesh@users.noreply.github.com>
2025-08-14 15:19:00 +0530

    31432

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length = 255)
+    phone_number = models.CharField(max_length = 20)
 
     def __str__(self):
         return self.name
