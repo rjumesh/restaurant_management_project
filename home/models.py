fcb2b1aa34f2ea10df51c7ff4491a8050799fb9a
umesh jadhav <115002233+rjumesh@users.noreply.github.com>
2025-08-18 20:55:48 +0530

    35277

--- home/models.py
+++ home/models.py
@@ -16,3 +16,16 @@ class RestaurantLocation(models.Model):
 
     def __str__(self):
         return f"{self.adress}, {self.city}, {self.state} - {self.zip_code}"
+
+
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menu_items')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
