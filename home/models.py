74b75a626a5d83e28362974c49e56c3ad7d74aff
umesh jadhav <115002233+rjumesh@users.noreply.github.com>
2025-08-18 18:41:38 +0530

    35279

--- home/models.py
+++ home/models.py
@@ -6,3 +6,13 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 100)
+    state = models.CharField(max_length = 100)
+    zip_code = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return f"{self.adress}, {self.city}, {self.state} - {self.zip_code}"
